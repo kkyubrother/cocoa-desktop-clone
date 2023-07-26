acae7609928c6db7e81415b467f1149cbef35ddc
kkyubr <kkyubrother@naver.com>
2023-07-26 11:44:12 +0900

    Updates

--- _next/static/chunks/app/chat/[id]/page-0beed8ef4cd8755b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[786],{9910:function(e,_,n){Promise.resolve().then(n.t.bind(n,3824,23))},3824:function(e){e.exports={bubble:"page_bubble__YNWG5",reverse_bubble:"page_reverse_bubble__WcKan",btn_send:"page_btn_send__i_nHR"}}},function(e){e.O(0,[253,698,744],function(){return e(e.s=9910)}),_N_E=e.O()}]);
