8a11dc42f5883726f48f2e89f02923938898a3dd
kkyubr <kkyubrother@naver.com>
2023-09-06 02:26:24 +0900

    채팅 페이지 내용 제작

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
-const repository = "cocoa-desktop-clone"
-const nextConfig = {
-  basePath: debug ? "" : `/${repository}`,
-  output: "export",
-  images: { unoptimized: debug }
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
